2af325679f004cbfd1c9e7edd3a98266a306c257
Denys Vasylenko <denys.vasylenko@uinno.io>
2025-09-03 12:09:35 +0300

    same with tracing

--- main.py
+++ main.py
@@ -28,8 +28,8 @@ def main():
         input_variables=["information"], template=summary_template
     )
 
-    #llm = ChatOpenAI(temperature=0, model="gpt-3.5-turbo")
-    llm = ChatOllama(temperature=0, model="gemma3:270m")
+    llm = ChatOpenAI(temperature=0, model="gpt-3.5-turbo")
+    #llm = ChatOllama(temperature=0, model="gemma3:270m")
     chain = summary_prompt_template | llm
 
     response = chain.invoke(input ={"information":information})
