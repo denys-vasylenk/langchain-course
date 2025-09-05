a8ee62f131ac429cf46b6f1ca11586ee7c4b164a
Denys Vasylenko <denys.vasylenko@uinno.io>
2025-09-05 12:47:57 +0300

    token calc

---
+++ metadata_tracing.py
@@ -0,0 +1,16 @@
+from dotenv import load_dotenv
+load_dotenv()
+from langsmith import Client
+
+client = Client()
+
+
+run_id = "0bafaadc-9d89-419d-b435-64e448c85067"
+run = client.read_run(run_id)
+
+print(run.id)
+print(run.name)
+print(run.total_tokens)
+print(run.prompt_tokens)
+print(run.completion_tokens)
+print(run.total_cost)
