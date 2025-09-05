422a7e4a4aa3226436c5f1e3b62cfad618eb9680
Denys Vasylenko <denys.vasylenko@uinno.io>
2025-09-05 17:52:01 +0300

    OtputParser

--- main.py
+++ main.py
@@ -1,28 +1,47 @@
 from dotenv import load_dotenv
-load_dotenv()
-
 from langchain import hub
 from langchain.agents import AgentExecutor
 from langchain.agents.react.agent import create_react_agent
+from langchain_core.output_parsers.pydantic import PydanticOutputParser
+from langchain_core.prompts import PromptTemplate
+from langchain_core.runnables import RunnableLambda
 from langchain_openai import ChatOpenAI
 from langchain_tavily import TavilySearch
+
+from prompt import REACT_PROMPT_WITH_FORMAT_INSTRUCTIONS
+from schema import AgentResponse
+
+load_dotenv()
 
 tools = [TavilySearch()]
 llm = ChatOpenAI(model="gpt-3.5-turbo", temperature=0)
 react_prompt = hub.pull("hwchase17/react")
+
+output_parser = PydanticOutputParser(pydantic_object=AgentResponse)
+react_prompt_with_format_instructions = PromptTemplate(
+    template=REACT_PROMPT_WITH_FORMAT_INSTRUCTIONS,
+    input_veraibles=[
+        "tools",
+        "tool_names",
+        "input",
+        "format_instructions",
+        "agent_scratchpad",
+    ],
+).partial(format_instructions=output_parser.get_format_instructions())
+
 agent = create_react_agent(
-    llm=llm,
-    tools=tools,
-    prompt=react_prompt
-    )
-agent_executor = AgentExecutor(agent=agent, tools=tools, verbouse=True)
+    llm=llm, tools=tools, prompt=react_prompt_with_format_instructions
+)
+agent_executor = AgentExecutor(
+    agent=agent, tools=tools, handle_parsing_errors=True, verbose=True
+)
 chain = agent_executor
 
 
 def main():
     result = chain.invoke(
-        input = {
-            "input":"search for 3 job position for an AI engeneer using LangChain in the Kyiv on linkedin and list their details",
+        input={
+            "input": "search for 3 job position for an AI engeneer using LangChain in the Kyiv on linkedin and list their details",
         }
     )
     print(result)
