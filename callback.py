b3b05c146b10f7648d434701e5e624b3b59ffd34
Denys Vasylenko <denys.vasylenko@uinno.io>
2025-09-09 14:11:19 +0300

    ReAct loop agent

---
+++ callback.py
@@ -0,0 +1,24 @@
+from typing import Any, Dict, List
+
+from langchain.callbacks.base import BaseCallbackHandler
+from langchain.schema import LLMResult
+
+
+class AgentCallbackHandler(BaseCallbackHandler):
+    def on_llm_start(
+        self, serialized: Dict[str, Any], prompts: List[str], **kwargs: Any
+    ) -> Any:
+        """Run when LLM starts running."""
+        print(f"***Prompt to LLM was:***\n{prompts[0]}")
+        print("********************")
+
+        pass
+
+    def on_llm_end(
+        self,
+        response: LLMResult,
+        **kwargs: Any,
+    ) -> Any:
+        """Run when LLM ends running."""
+        print(f"***LLM response:***\n{response.generations[0][0].text}")
+        pass
---
+++ main.py
@@ -0,0 +1,102 @@
+from typing import List, Union
+
+from dotenv import load_dotenv
+from langchain.agents import tool
+from langchain.agents.format_scratchpad import format_log_to_str
+from langchain.agents.output_parsers import ReActSingleInputOutputParser
+from langchain.prompts import PromptTemplate
+from langchain.schema import AgentAction, AgentFinish
+from langchain.tools import Tool
+from langchain.tools.render import render_text_description
+from langchain_core.tools import render_text_description
+from langchain_openai.chat_models import ChatOpenAI
+
+load_dotenv()
+
+from callback import AgentCallbackHandler
+
+
+@tool
+def get_text_length(text: str) -> int:
+    """Returns the length of the text by characters"""
+    print(f"get_text_length enter with {text=}")
+    text = text.strip("'\n").strip('"')
+    return len(text)
+
+
+def find_tool_by_name(tools: List[Tool], tool_name: str) -> Tool:
+    for tool in tools:
+        if tool.name == tool_name:
+            return tool
+    raise ValueError(f"Tool with name {tool_name} not found")
+
+
+if __name__ == "__main__":
+    print("Hello React LangChain")
+    tools = [get_text_length]
+
+    template = """
+    Answer the following questions as best you can. You have access to the following tools:
+
+    {tools}
+
+    Use the following format:
+
+    Question: the input question you must answer
+    Thought: you should always think about what to do
+    Action: the action to take, should be one of [{tool_names}]
+    Action Input: the input to the action
+    Observation: the result of the action
+    ... (this Thought/Action/Action Input/Observation can repeat N times)
+    Thought: I now know the final answer
+    Final Answer: the final answer to the original input question
+
+    Begin!
+
+    Question: {input}
+    Thought: {agent_scratchpad}
+    """
+
+    prompt = PromptTemplate.from_template(template=template).partial(
+        tools=render_text_description(tools),
+        tool_names=", ".join([t.name for t in tools]),
+    )
+
+    llm = ChatOpenAI(
+        temperature=0,
+        stop=["\nObservation:", "Observation", "Observation:"],
+        callbacks=[AgentCallbackHandler()],
+    )
+
+    agent = (
+        {
+            "input": lambda x: x["input"],
+            "agent_scratchpad": lambda x: format_log_to_str(x["agent_scratchpad"]),
+        }
+        | prompt
+        | llm
+        | ReActSingleInputOutputParser()
+    )
+
+    intermidiate_steps = []
+    agent_step = ""
+    while not isinstance(agent_step, AgentFinish):
+        agent_step: Union[AgentAction, AgentFinish] = agent.invoke(
+            {
+                "input": "what is the text leng of 'Dog' in characters?",
+                "agent_scratchpad": intermidiate_steps,
+            }
+        )
+        print(agent_step)
+
+        if isinstance(agent_step, AgentAction):
+            tool_name = agent_step.tool
+            tool_to_use = find_tool_by_name(tools, tool_name)
+            tool_input = agent_step.tool_input
+
+            observation = tool_to_use.func(str(tool_input))
+            print(f"{observation=}")
+            intermidiate_steps.append((agent_step, str(observation)))
+
+    if isinstance(agent_step, AgentFinish):
+        print(agent_step.return_values)
